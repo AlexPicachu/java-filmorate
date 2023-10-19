5123c7fe1dd71509e897698ddde4d78b6456e86d
Alexander <a.v.golov@yandex.ru>
2023-10-19 15:02:27 +0300

    ТЗ 10 спринт

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -3,10 +3,14 @@ package ru.yandex.practicum.filmorate.storage;
 import ru.yandex.practicum.filmorate.model.User;
 
 import java.util.List;
+
 //интерфейс пользователей
 public interface UserStorage {
     User getUserById(int id);
+
     List<User> getUserMap();
+
     User createUser(User user);
+
     User updateUsers(User user);
 }
